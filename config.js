26aec1955320e93a4e0b3bb0dcb5b6b461f6f951
Vichoe <vicente.degavardo@estudiante.uc.cl>
2026-06-23 21:07:16 -0400

    aaaaa

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ Protobject.setProduction(true);
 Protobject.initialize([
   {
     name: "Pantalla",
-    page: "fisicalizacion.html",
+    page: "fisicalizacion.html", // <-- Tiene que decir esto, NO index.html
     main: true,
     debug: "master",
   },

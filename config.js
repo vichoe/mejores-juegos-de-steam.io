46d23caa17d9d7546c5f78d499817d14a5563570
Vichoe <vicente.degavardo@estudiante.uc.cl>
2026-06-23 20:53:03 -0400

    aaaaaaaa

---
+++ config.js
@@ -0,0 +1,15 @@
+Protobject.setProduction(true);
+
+Protobject.initialize([
+  {
+    name: "Pantalla",
+    page: "fisicalizacion.html",
+    main: true,
+    debug: "master",
+  },
+  {
+    name: "Sensor",
+    page: "sensor.html",
+    debug: "local",
+  }
+]);
